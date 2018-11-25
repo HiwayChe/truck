c31ff2955c6b282b73f74fba5ca9cb80d677534a
guangai.che <guangai.che@ydcfo.com>
2018-11-26 00:33:48 +0800

    fix

--- truck-common/src/main/java/com/dong/common/model/BaseModel.java
+++ truck-common/src/main/java/com/dong/common/model/BaseModel.java
@@ -1,7 +1,5 @@
 package com.dong.common.model;
 
-import lombok.Data;
-
 import java.io.Serializable;
 import java.util.Date;
 
@@ -9,7 +7,6 @@ import java.util.Date;
  * @author cheguangai
  * @date 2018/11/25 0025
  */
-@Data
 public class BaseModel implements Serializable {
 	private static final long serialVersionUID = 1L;
 
@@ -18,4 +15,44 @@ public class BaseModel implements Serializable {
 	private Date createDate;
 	private String updateBy;
 	private Date updateDate;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCreateBy() {
+		return createBy;
+	}
+
+	public void setCreateBy(String createBy) {
+		this.createBy = createBy;
+	}
+
+	public Date getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+
+	public String getUpdateBy() {
+		return updateBy;
+	}
+
+	public void setUpdateBy(String updateBy) {
+		this.updateBy = updateBy;
+	}
+
+	public Date getUpdateDate() {
+		return updateDate;
+	}
+
+	public void setUpdateDate(Date updateDate) {
+		this.updateDate = updateDate;
+	}
 }
